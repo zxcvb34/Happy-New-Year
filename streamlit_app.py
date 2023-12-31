6d0ef446ff3c67137ac2a2272ae7bb95b573c1e9
unknown <annaszakky12@gmail.com>
2023-12-31 21:06:49 +0700

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,98 @@
+from pathlib import Path
+import json
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_extras.let_it_rain import rain
+
+# Directories and file paths
+THIS_DIR = Path(__file__).parent
+CSS_FILE = THIS_DIR / "style" / "style.css"
+ASSETS = THIS_DIR / "assets"
+LOTTIE_ANIMATION = ASSETS / "Animation - 1704023561643.json"
+
+
+# Function to load and display the Lottie animation
+def load_lottie_animation(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+
+# Function to apply snowfall effect
+def run_snow_animation():
+    rain(emoji="🎊", font_size=20, falling_speed=5, animation_length="infinite")
+
+
+# Function to get the name from query parameters
+def get_person_name():
+    query_params = st.experimental_get_query_params()
+    return query_params.get("name", ["Friend"])[0]
+
+
+# Page configuration
+st.set_page_config(page_title="Happy New Year", page_icon="🗓")
+
+# Run snowfall animation
+run_snow_animation()
+
+# Apply custom CSS
+with open(CSS_FILE) as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# Display header with personalized name
+PERSON_NAME = get_person_name()
+st.header(f"Happy New Year, {PERSON_NAME}! 🗓", anchor=False)
+
+# Display the Lottie animation
+lottie_animation = load_lottie_animation(LOTTIE_ANIMATION)
+st_lottie(lottie_animation, key="lottie-holiday", height=300)
+
+# Personalized holiday message
+st.markdown(
+    f"Dear {PERSON_NAME}, As the New Year unfolds, may it bring you prosperity, good health, and boundless happiness. Happy 2024!. 🌟"
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
